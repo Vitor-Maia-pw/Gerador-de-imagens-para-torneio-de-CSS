27125154b4fa4d5852e9322ff8a8ace77678a0f8
Vitor-Maia-pw <vitormaiatls@gmail.com>
2026-04-03 10:50:07 -0400

    atualização layout1 header

--- script.js
+++ script.js
@@ -11,9 +11,10 @@ canvas.style.height = "1080px";
 ctx.scale(scale, scale);
 
 //tamanho minimo que um objeto pode ter de W ou H
-let tamMin = 100
+let tipoHeader = 0
+let larguraMax = 1920
 let value = 0
-
+let aleatorio = 0
 // função que seleciona um dos layouts que serviram para limitar os objetos
 function escolheLayout(l) {
     switch (l) {
@@ -31,44 +32,150 @@ escolheLayout(1)
 function layout1() {
     // a parte superior do layout um, sua primeira parte
     // ele que contem o mapa para criar os objetos dentro da parte 1
-    
-    let arrayElem1 = [] 
+
+    let arrayElem1 = []
     // cria de forma visivel as demarcações do layout, os lugares que devem ter quadrados sem vazar
     ctx.beginPath()
     ctx.fillStyle = "blue"
-    ctx.fillRect(0, 0, 1920, 270)
+    ctx.fillRect(0, 0, larguraMax, 270)
     ctx.fillStyle = "red"
-    ctx.fillRect(0, 270, 1920, 540)
+    ctx.fillRect(0, 270, larguraMax, 540)
     ctx.fillStyle = "green"
-    ctx.fillRect(0, 810, 1920, 270)
-    // chama a função, e envia o "mapa" das partes necessarias
-    criarElem(0, 0, 1920, 270, "black")
+    ctx.fillRect(0, 810, larguraMax, 270)
+
+    tipoHeader = Math.random() * 2
+    // chama a função, e envia o tipo de layout do header
+    escolheHeader(1, larguraMax, 270)
 
 }
-// xi, yi = coordenadas iniciais(minimas) xf, yf = coordenadas finais(maximas) 
-// w, h = tamanho maximo width e height que pode chegar o objeto
-function criarElem(xi, yi, xf, yf, color) {
-    // limita e gera um valor aleatorio conforme as informações enviadas pela part enviada
-    let x = Math.random() * (xf - xi - tamMin) + xi
-    let y = Math.random() * (yf - yi - tamMin) + yi
-    let w = Math.random() * (xf - tamMin - x) + tamMin
-    let h = Math.random() * (yf - tamMin - y) + tamMin
+
+function escolheHeader(tipo, w, h) {
+    //verifica qual o tipo do header e chama a função
+    switch (tipo) {
+        case 1:
+            // envia o tamanho do objeto
+            criarHeader1(w, h)
+            break;
+
+        default:
+            break;
+    }
+}
+
+function criarHeader1(wHe, hHe, x, y, xf, yf, tamMinX, tamMinY, w, h, cx, cy, ch, cw) {
+
+    //gera um valor aleatorio para x que vai de 0 a 60
+    // esse é a distancia do objeto até a borda esquerda
+    x = Math.random() * 6 * 10
+    y = parseInt(Math.random() * 10) * 10
+
+    // valor limite usado para limitar o tamanho do objeto, ele não pode ser maior que isso
+    xf = wHe / 4
+    yf = hHe - y
+
+    // tamanho minimo do objeto, as vezes pode ser menor
+    tamMinX = hHe - y * 2
+    tamMinY = hHe - y * 2
+
+    // gera o tamanho de forma aleatoria
+    w = Math.random() * (xf - tamMinX - x) + tamMinX
+    h = Math.random() * (yf - tamMinY - y) + tamMinY
+
+    // salva os tamanhos para usalos posteriormente
+    cx = x
+    cy = y
+    cw = w
+    ch = h
+
+    //envia as características para serem desenhadas
+    criarElem(x, y, w, h, "black")
+    escolheRet(x, y, w + x, h + y)
+
+    //chama o segundo quadrado do centro
+    x = x + w + parseInt(Math.random() * 20) * 10
+    y = parseInt(Math.random() * 9) * 10;
+    xf = wHe - x * 2
+    tamMinX = wHe - x * 2
+    tamMinY = hHe - y * 2
+
+    w = tamMinX
+    h = Math.random() * (yf - tamMinY - y) + tamMinY
+
+    //desenha o segundo quadrado
+    criarElem(x, y, w, h, "purple")
+    escolheRet(x, y, w + x, h + y)
     
+    // espelha o primeiro quadrado do outro lado
+    x = wHe - cx - cw
+    y = cy
+    
+    criarElem(x, y, cw, ch, "black")
+    escolheRet(x, y, cw + x, ch + y)
+}
+
+function escolheRet(xi, yi, xf, yf, x, y, w, h, tamMin) {
+    aleatorio = Math.floor(Math.random() * 5)
+    switch (aleatorio) {
+        case 0:
+
+            break;
+        case 1:
+            tamMinX = Math.floor((xf - xi) / 5)
+            tamMinY = Math.floor((yf - yi) / 5)
+            x = xi + Math.floor(Math.random() * (20 - 10) + 5)
+            y = yi + x - xi
+            w = Math.random() * ((xf - x) - tamMinX) + tamMinX
+            h = Math.random() * ((yf - y) - tamMinY) + tamMinY
+            criarElem(x, y, w, h, "green")
+            break;
+        case 2:
+            tamMinX = Math.floor((xf - xi) / 5)
+            tamMinY = Math.floor((yf - yi) / 5)
+            x = xi
+            y = yi
+            w = Math.random() * ((xf - x)/2 - tamMinX) + tamMinX
+            h = Math.random() * ((yf - y)/2 - tamMinY) + tamMinY
+            criarElem(x, y, w, h, "green")
+            
+            x = xf - w
+            y = yf - h
+            criarElem(x, y, w, h, "white")
+        break;
+        case 3:
+            tamMinX = Math.floor((xf - xi) / 5)
+            tamMinY = Math.floor((yf - yi) / 5)
+            x = xi
+            y = yi
+            w = Math.random() * ((xf - x)/2 - tamMinX) + tamMinX
+            h = Math.random() * ((yf - y)/2 - tamMinY) + tamMinY
+            criarElem(x, y, w, h, "green")
+            
+            x = xf - w
+            criarElem(x, y, w, h, "white")
+        break;
+        case 4:
+            tamMinX = Math.floor((xf - xi) / 5)
+            tamMinY = Math.floor((yf - yi) / 5)
+            w = Math.random() * ((xf - xi)/2 - tamMinX) + tamMinX
+            h = Math.random() * ((yf - yi)/2 - tamMinY) + tamMinY
+// x deve ser um valor de xi + metade do w do retangulo, menos metade do w do retangulo menor
+            x = xi + (xf - xi)/2 - w/2
+            y = yi + (yf - yi)/2 - h/2
+            criarElem(x, y, w, h, "white")
+
+        break;
+        case 5:
+
+        break;
+    }
+}
+
+function criarElem(x, y, w, h, color) {
+
     ctx.beginPath()
     ctx.fillStyle = color
     ctx.fillRect(x, y, w, h)
 
-    if(x + w < xf - tamMin ){
-        criarElem((x + w), yi, xf, yf, "red")
-        
-    }
-    
-    // verifica se irá gerar um elemnto dentro do elemento anterior
-    if (value === 0) {
-        addElem = Math.random() * 2
-        value++
-        if (addElem < 1) {
-            criarElem(x, y, (x + w), (y + h), "green")
-        }
-    }
 }
+// x = Math.random() * (xf - xi - tamMinX) + xi;
+// y = Math.random() * (yf - yi - tamMinY) + yi;
