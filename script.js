192bbc38ccacbcd9a75e92aa7589c8d4239c3d1e
Vitor-Maia-pw <vitormaiatls@gmail.com>
2026-07-10 07:43:23 -0400

    adicao de quadrados

--- script.js
+++ script.js
@@ -13,6 +13,8 @@ let x = 0
 let y = 0
 let porcentagem = 0
 let a = 0
+let tipoRet = 0
+let aleatorio = 0
 function resizeCanvas() {
     const scale = window.devicePixelRatio;
 
@@ -38,19 +40,27 @@ function resizeCanvas() {
 
 window.addEventListener("resize", resizeCanvas);
 resizeCanvas();
-
+/* A layer serve para organizar os elementos em camadas diferentes, assim os elementos da layer 1 foram criador a partir dos 
+elementos da layer 0 */
 function EscolheLayout (layer, x, y, w, h, W, H, direction, id) {
     if (!objetosArray[layer]) {
         objetosArray[layer] = [];
     }
+/* O Id é importante, pois quando o VerificaDiv for chamado, ele receberá um elemento [[[][]]] assim quando ele for dividir um 
+elemento, ele vai selecionar a layer, e terá o id para não verificar duas vezes o elemento da mesma layer, assim ficando:
+1. divide o 1° elemento da layer 0, e chama o VerificaDiv com a layer 1 e id 0
+2. divide o 2° elemento da layer 0, e chama o VerificaDiv com a layer 1 e id 1
+dessa forma quando o VerificaDiv for chamado para dividir o elemento id 0 da layer 1,
+ele não irá verificar o elemento id 1 da layer 1, e quando for chamado para dividir o elemento id 1 da layer 1,
+ele não irá verificar o elemento id 0 da layer 1, assim evitando que um elemento seja dividido mais de uma vez*/
     if (!objetosArray[layer][id]) {
         objetosArray[layer][id] = [];
     }
     // direction = row
     if (direction === 0) {
         nDiv = W / 192
-        if (nDiv > 4) {
-            nDiv = 4
+        if (nDiv > 3) {
+            nDiv = 3
         }
         quant = Math.floor(Math.random() * (nDiv - 1)) + 1
         nDiv = 1
@@ -62,7 +72,8 @@ function EscolheLayout (layer, x, y, w, h, W, H, direction, id) {
                 w = W
             }
             else {
-                w = Math.floor(Math.random() * ((W / (quant + nDiv)) - 192)) + 192
+                // tem a chance de gerar 
+                w = Math.floor(Math.random() * (W - (192 * (quant - i + 1)))) + 192
                 W -= w  
                 nDiv--
             }
@@ -77,8 +88,8 @@ function EscolheLayout (layer, x, y, w, h, W, H, direction, id) {
     //direction = column
     else {
         nDiv = H / 192
-        if (nDiv > 4) {
-            nDiv = 4
+        if (nDiv > 3) {
+            nDiv = 3
         }
         quant = Math.floor(Math.random() * (nDiv - 1)) + 1
         nDiv = 1
@@ -90,10 +101,10 @@ function EscolheLayout (layer, x, y, w, h, W, H, direction, id) {
                 h = H
             }
             else {
-                h = Math.floor(Math.random() * ((H/ (quant + nDiv)) - 192)) + 192
+                h = Math.floor(Math.random() * (H - (192 * (quant - i + 1)))) + 192
                 H -= h  
-                nDiv--
             }
+        
             objetosArray[layer][id].push({
                 x: x,
                 y: y,
@@ -109,6 +120,7 @@ function EscolheLayout (layer, x, y, w, h, W, H, direction, id) {
     }
 }
 
+let ultimaVolta = 0
 function VerificaDiv(layer, direction, id) {
     let divisoes = 0
     for (let i = 0; i < objetosArray[layer][id].length; i++) {
@@ -116,23 +128,80 @@ function VerificaDiv(layer, direction, id) {
         if (direction === 0) {
             if (objetosArray[layer][id][i].h > 192 * 2) {
                 porcentagem = (objetosArray[layer][id][i].h / 192)
-                dividir = Math.floor(Math.random() * porcentagem) + (porcentagem < 5 ? 0 : 2)  
+                dividir = Math.floor(Math.random() * porcentagem) + (porcentagem < 3 ? 0 : 2)  
                 
             }
         }else{
             if (objetosArray[layer][id][i].w > 192 * 2) {
                 porcentagem = (objetosArray[layer][id][i].w / 192)
-                dividir = Math.floor(Math.random() * porcentagem) + (porcentagem < 5 ? 0 : 2)  
+                dividir = Math.floor(Math.random() * porcentagem) + (porcentagem < 3 ? 0 : 2)  
                 
             }
         }
-        if (dividir > 1) {
-            EscolheLayout(layer + 1, objetosArray[layer][id][i].x, objetosArray[layer][id][i].y, 0, 0, objetosArray[layer][id][i].w, objetosArray[layer][id][i].h, direction === 0 ? 1 : 0, i)
+        
+        if (dividir > 0) {
+            aleatorio = Math.floor(Math.random() * 2)
+            if (aleatorio === 0) {
+                EscolheLayout(layer + 1, objetosArray[layer][id][i].x, objetosArray[layer][id][i].y, 0, 0, objetosArray[layer][id][i].w, objetosArray[layer][id][i].h, direction === 0 ? 1 : 0, i)
+                
+            }else{
+                AddRet(layer, id, i)
+            }
+            // se for o primeiro elemento da função, é impossivel a layerAtual ser diferente de layer
+            // se o id for maior que 0, a layerAtual pode ser diferente de layer
+            // se id for 0, e não tiver mais IDs ele pode ser diferente de layer
+            // 
             divisoes++
         }
+        if(layer === 0 && i === objetosArray[layer][id].length - 1 ) {
+            ultimaVolta++
+        }
     }
-    if (divisoes === 0) {
+    if(layer === 0){
         geraCor()
+        console.log(objetosArray)
+       
+    }
+    if (layer  === objetosArray.length - 1 && id === objetosArray[layer].length - 1 && divisoes === 0) {
+    }
+}
+
+function AddRet(layer, id, obj) {
+    if (!objetosArray[layer + 1]) {
+        objetosArray[layer + 1] = [];
+    }
+
+    if (!objetosArray[layer + 1][id + 1]) {
+        objetosArray[layer + 1][id + 1] = [];
+    }
+
+    W = objetosArray[layer][id][obj].w
+    H = objetosArray[layer][id][obj].h
+    x = objetosArray[layer][id][obj].x
+    y = objetosArray[layer][id][obj].y
+    direction = Math.floor(Math.random() * 2)
+    // if (w > 192 * 2 + 50 && h > 192 * 2 + 50) {
+    //     tipoRet = Math.floor(Math.random() * 2)
+    // }
+    switch (tipoRet) {
+        case 0:
+            w = Math.floor(Math.random() * (W/2) + W/2)
+            h = Math.floor(Math.random() * (H - H * 7 / 10) + H * 7 / 10)
+            
+            y = Math.floor(Math.random() * (H - h) + y)
+            objetosArray[layer + 1][id + 1].push({
+                x: x,
+                y: y,
+                w: w,
+                h: h,
+            })
+            
+            VerificaDiv(layer + 1, direction, id )
+            break;
+    
+        case 1:
+
+            break;
     }
 }
 
@@ -166,3 +235,19 @@ function criarElem(x, y, w, h, cor) {
 }
 // x = Math.random() * (xf - xi - tamMinX) + xi;
 // y = Math.random() * (ch - tamMinY) + yi;
+// 6 9 12
+/*
+g: 6
+m: 9
+p: 12
+
+G: 6
+M: 9
+P: 12
+
+g / G = 83
+m / M =
+p / P =
+
+300 100 = p 200 = m 283 = g
+*/
